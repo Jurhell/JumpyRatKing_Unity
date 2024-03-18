a913e191839736f3cb46a74c77ff8f65eb22df3a
Jurhell Hughes <somethinghughes@gmail.com>
2024-03-18 16:58:46 -0500

    Added prefabs and better controls

--- Assets/Scripts/BetterPlayerController.cs
+++ Assets/Scripts/BetterPlayerController.cs
@@ -9,9 +9,18 @@ public class BetterPlayerController : MonoBehaviour
     private float _maxSpeed;
     [SerializeField]
     private float _acceleration;
+    [SerializeField]
+    private float _jumpHeight;
+    [Space]
+    [SerializeField]
+    private Vector3 _groundCheck;
+    [SerializeField]
+    private float _groundCheckRadius;
 
     private Rigidbody _rigidbody;
     private Vector3 _moveDirection;
+    private bool _jumpInput = false;
+    private bool _isGrounded = false;
 
     void Awake()
     {
@@ -23,15 +32,38 @@ public class BetterPlayerController : MonoBehaviour
     {
         // Get movement force
         _moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+        _jumpInput = Input.GetAxisRaw("Jump") != 0;
     }
 
     void FixedUpdate()
     {
+        // Ground check
+        _isGrounded = Physics.OverlapSphere(transform.position + _groundCheck, _groundCheckRadius).Length > 1;
+
         // Add movement force
         _rigidbody.AddForce(_moveDirection * _acceleration * Time.fixedDeltaTime, ForceMode.VelocityChange);
 
         // Clamp velocity to max speed
-        if (_rigidbody.velocity.magnitude > _maxSpeed)
-            _rigidbody.velocity = _rigidbody.velocity.normalized * _maxSpeed;
+        Vector3 velocity = _rigidbody.velocity;
+        float newXSpeed = Mathf.Clamp(_rigidbody.velocity.x, -_maxSpeed, _maxSpeed);
+        velocity.x = newXSpeed;
+        _rigidbody.velocity = velocity;
+
+        // Add jump force
+        if (_jumpInput && _isGrounded)
+        {
+            // Calculate force needed to reach _jumpHeight
+            float force = Mathf.Sqrt(_jumpHeight * -2f * Physics.gravity.y);
+            _rigidbody.AddForce(Vector3.up * force, ForceMode.Impulse);
+        }
     }
+
+#if UNITY_EDITOR
+    void OnDrawGizmosSelected()
+    {
+        // Draw ground check
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position + _groundCheck, _groundCheckRadius);
+    }
+#endif
 }
